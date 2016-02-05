9a34cecfb0d8bfad576938e372f09e28b1604589
Katy Alexandrova <Katy Alexandrova>
2016-02-05 20:37:59 +0000

    Chapter 5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -81,3 +81,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    "C:\Users\Katy Aleksandrova\code\tango_with_django_project",
+    TEMPLATE_PATH,
+)
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = ("C:\Users\Katy Aleksandrova\code\tango_with_django_project",
+    STATIC_PATH,
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
